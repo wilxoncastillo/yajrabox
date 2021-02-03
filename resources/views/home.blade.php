82062d6d9da224600ce1706a857a04fe5922f823
wilxoncastillo <wilxoncastillo@gmail.com>
2021-02-03 12:32:46 -0300

    Imstalacion de datatables.net-dt, yajra/laravel-datatables creacion de la plantailla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -14,10 +14,49 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table" id='myTable'>
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Name</th>
+                          <th>Email</th>
+                          <th>Created_At</th>
+                          <th>Updated_At</th>
+
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @forelse ($users as $item)
+                        <tr>
+                          <th scope="row">{{ $item->id }}</th>
+                          <td>{{ $item->name }}</td>
+                          <td>{{ $item->email }}</td>
+                          <td>{{ $item->created_at }}</td>
+                          <td>{{ $item->updated_at }}</td>
+                        </tr>
+                        @empty
+                            <div class="alert-info">
+                                No hay Datos
+                            </div>
+                        @endforelse
+                      </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+@push('scripts')
+    <!--
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    -->
+
+    <script type="text/javascript">
+        $(document).ready( function () {
+            $('#myTable').DataTable();
+        });
+    </script>
+@endpush
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,18 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
@@ -76,5 +79,10 @@
             @yield('content')
         </main>
     </div>
+
+
+
+    @stack('scripts')
+
 </body>
 </html>
