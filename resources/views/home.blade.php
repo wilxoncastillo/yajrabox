3be19372d3b3e29716c04852bf9e7e1f71671250
wilxoncastillo <wilxoncastillo@gmail.com>
2021-02-05 09:30:08 -0300

    ocultando columnas por defecto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,15 +14,14 @@
                         </div>
                     @endif
                     <div>
-                        Toggle column:
-                        <a class="toggle-vis" data-column="0">id</a> -
-                        <a class="toggle-vis" data-column="1">nombre</a> -
-                        <a class="toggle-vis" data-column="2">intro</a> -
-                        <a class="toggle-vis" data-column="3">email</a> -
-                        <a class="toggle-vis" data-column="4">created_at</a> -
-                        <a class="toggle-vis" data-column="5">update_at</a>
-
-                    </div>
+                        <span class="font-weight-bold">Alternar Columnas:</span>
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="0">Id</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="1">Nombre</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="2">Intro</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="3">Email</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="4">Created_At</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="5">Update_At</a>
+                    </div><hr>
 
                     <table class="table" id='myTable'>
                       <thead>
@@ -63,15 +62,10 @@
                 ]
             });
 
-            /*
-            var table = $('#myTable').DataTable( {
-                "scrollY": "200px",
-                "paging": false
-            } );
-            */
-
             $('a.toggle-vis').on( 'click', function (e) {
                 e.preventDefault();
+
+                //console.log($(this).attr('data-column'));
 
                 // Get the column API object
                 var column = table.column( $(this).attr('data-column') );
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,14 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <div>
+                        <span class="font-weight-bold">Alternar Columnas:</span>
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="1">Id</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="2">Nombre</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="3">Email</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="4">Created_At</a> -
+                        <a class="toggle-vis btn btn-sm btn-primary" data-column="5">Update_At</a>
+                    </div><hr>
 
                     {{ $dataTable->table([], true) }}
 
@@ -25,4 +33,27 @@
 
 @push('scripts')
     {{ $dataTable->scripts() }}
+
+    <script type="text/javascript">
+        $(document).ready( function () {
+            var table = $('#user-table').DataTable();
+
+            var column = table.column(4);
+            column.visible( ! column.visible() );
+
+            var column = table.column(5);
+            column.visible( ! column.visible() );
+
+
+            $('a.toggle-vis').on( 'click', function (e) {
+                e.preventDefault();
+
+                // Get the column API object
+                var column = table.column( $(this).attr('data-column') );
+
+                // Toggle the visibility
+                column.visible( ! column.visible() );
+            } );
+        });
+    </script>
 @endpush
